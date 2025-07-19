a3aafc9d0d9ce3b0b15cba0b3287d20b9028bc8c
Sachin <145639140+Derikklok@users.noreply.github.com>
2025-07-19 20:22:37 +0530

    Add variable examples and type printing to app.py

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
 # Python comments
 print('Hello World!')
+
+# Python variables
+# Python has no specific way to create variables.
+# Variables are created when values are assigned to it
+
+num1 = 4 # Int
+name = "john" # String
+isLoading = True # bool
+price = 34.88 # float
+
+print("Integer:\t",num1)
+print("String:\t\t",name)
+print("Boolean:\t",isLoading)
+print("Float:\t\t",price)
+
+print(type(num1))
+print(type(name))
+print(type(isLoading))
+print(type(price))
